a50ed8ed3ca115d2580b1888f5cb149ff11f6d21
ptya <petr.szab@gmail.com>
2019-11-15 22:06:37 +0100

    Contact clean up

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,39 +1,16 @@
-import React, { useState, useLayoutEffect } from 'react'
-import PropTypes from 'prop-types'
-import { Link } from 'gatsby'
-import styled from 'styled-components'
+import React from 'react'
 import Title from 'components/elements/Title'
 import Form from './Form'
+import Main from './styles/Main'
 
-const Main = styled.main`
-  display: grid;
-  justify-content: center;
-  align-items: center;
+const About = () => (
+  <Main>
+    <Title>Get in touch</Title>
+    <p>Have a question? Want to work together or just say hi?</p>
+    <Form />
+  </Main>
+)
 
-  h1 {
-    justify-self: baseline;
-    left: -50px;
-  }
-
-  p {
-    margin: 10px 25px;
-    font-size: 1.8rem;
-    font-weight: 200;
-  }
-`
-
-const About = () => {
-  const [active, setActive] = useState([])
-  const [hovered, setHovered] = useState([])
-
-  return (
-    <Main>
-      <Title>Get in touch</Title>
-      <p>Have a question? Want to work together or just say hi?</p>
-      <Form />
-    </Main>
-  )
-}
 About.propTypes = {}
 
 export default About
---
+++ src/components/Contact/styles/Main.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+const Main = styled.main`
+  display: grid;
+  justify-content: center;
+  align-items: center;
+
+  h1 {
+    justify-self: baseline;
+    left: -50px;
+  }
+
+  p {
+    margin: 10px 25px;
+    font-size: 1.8rem;
+    font-weight: 200;
+  }
+`
+
+export default Main
