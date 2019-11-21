f041f81b227ce85856b731c056dae374d0f27014
ptya <petr.szab@gmail.com>
2019-11-21 22:08:11 +0100

    Work component started

---
+++ src/components/Work/Work.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Work = () => {
+  const foo = 'bar'
+  // TODO
+  // 4 rows grid
+  //  - title
+  //  - text
+  //  - works grid
+  //  - text
+  return <div>hoi</div>
+}
+
+export default Work
