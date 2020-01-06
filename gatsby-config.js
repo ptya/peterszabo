1327f3d9c25c2e15f7e68692d14c8bc93c009f11
ptya <petr.szab@gmail.com>
2020-01-06 08:28:14 +0100

    Forgot react-helmet plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ const path = require('path')
 module.exports = {
   plugins: [
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 import PropTypes from 'prop-types'
-import { StaticQuery, graphql } from 'gatsby'
+import { StaticQuery, graphql, useStaticQuery } from 'gatsby'
 
 import favicon from 'assets/images/logo.ico'
 
@@ -19,51 +19,120 @@ const query = graphql`
   }
 `
 
-const SEO = ({ title, description, image, pathname, article }) => (
-  <StaticQuery
-    query={query}
-    render={({
-      site: {
-        siteMetadata: {
-          defaultTitle,
-          titleTemplate,
-          defaultDescription,
-          siteUrl,
-          defaultImage,
-        },
+// const SEO = ({ title, description, image, pathname, article }) => (
+//   <StaticQuery
+//     query={query}
+//     render={({
+//       site: {
+//         siteMetadata: {
+//           defaultTitle,
+//           titleTemplate,
+//           defaultDescription,
+//           siteUrl,
+//           defaultImage,
+//         },
+//       },
+//     }) => {
+//       const seo = {
+//         title: title || defaultTitle,
+//         description: description || defaultDescription,
+//         image: `${siteUrl}${image || defaultImage}`,
+//         url: `${siteUrl}${pathname || '/'}`,
+//       }
+//       return (
+//         <>
+//           <Helmet title={seo.title} titleTemplate={titleTemplate}>
+//             <link rel="icon" type="image/x-icon" href={favicon} />
+//             <meta name="description" content={seo.description} />
+//             <meta name="image" content={seo.image} />
+//             {seo.url && <meta property="og:url" content={seo.url} />}
+//             {(article ? true : null) && (
+//               <meta property="og:type" content="article" />
+//             )}
+//             {seo.title && <meta property="og:title" content={seo.title} />}
+//             {seo.description && (
+//               <meta property="og:description" content={seo.description} />
+//             )}
+//             {seo.image && <meta property="og:image" content={seo.image} />}
+//             <link
+//               href="https://api.tiles.mapbox.com/mapbox-gl-js/v1.4.1/mapbox-gl.css"
+//               rel="stylesheet"
+//             />
+//           </Helmet>
+//         </>
+//       )
+//     }}
+//   />
+// )
+
+const SEO = ({ title, description, image, pathname, article, lang, meta }) => {
+  const {
+    site: {
+      siteMetadata: {
+        defaultTitle,
+        titleTemplate,
+        defaultDescription,
+        siteUrl,
+        defaultImage,
       },
-    }) => {
-      const seo = {
-        title: title || defaultTitle,
-        description: description || defaultDescription,
-        image: `${siteUrl}${image || defaultImage}`,
-        url: `${siteUrl}${pathname || '/'}`,
+    },
+  } = useStaticQuery(
+    graphql`
+      query SEO {
+        site {
+          siteMetadata {
+            defaultTitle: title
+            titleTemplate
+            defaultDescription: description
+            siteUrl: url
+            defaultImage: image
+          }
+        }
       }
-      return (
-        <>
-          <Helmet title={seo.title} titleTemplate={titleTemplate}>
-            <link rel="icon" type="image/x-icon" href={favicon} />
-            <meta name="description" content={seo.description} />
-            <meta name="image" content={seo.image} />
-            {seo.url && <meta property="og:url" content={seo.url} />}
-            {(article ? true : null) && (
-              <meta property="og:type" content="article" />
-            )}
-            {seo.title && <meta property="og:title" content={seo.title} />}
-            {seo.description && (
-              <meta property="og:description" content={seo.description} />
-            )}
-            {seo.image && <meta property="og:image" content={seo.image} />}
-            <link
-              href="https://api.tiles.mapbox.com/mapbox-gl-js/v1.4.1/mapbox-gl.css"
-              rel="stylesheet"
-            />
-          </Helmet>
-        </>
-      )
-    }}
-  />
-)
+    `
+  )
+
+  const metaTitle = title || defaultTitle
+  const metaImage = `${siteUrl}${image || defaultImage}`
+  const metaUrl = `${siteUrl}${pathname || '/'}`
+  const metaDescription = description || defaultDescription
+
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang,
+      }}
+      title={metaTitle}
+      titleTemplate={`%s | ${titleTemplate}`}
+      meta={[
+        {
+          name: `description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:title`,
+          content: metaTitle,
+        },
+        {
+          property: `og:description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:type`,
+          content: `website`,
+        },
+        {
+          property: `og:image`,
+          content: metaImage,
+        },
+        {
+          property: `og:url`,
+          content: metaUrl,
+        },
+      ].concat(meta)}
+    />
+  )
+}
 
 SEO.propTypes = {
   title: PropTypes.string,
@@ -71,6 +140,8 @@ SEO.propTypes = {
   image: PropTypes.string,
   pathname: PropTypes.string,
   article: PropTypes.bool,
+  lang: PropTypes.string,
+  meta: PropTypes.arrayOf(PropTypes.object),
 }
 SEO.defaultProps = {
   title: null,
@@ -78,6 +149,8 @@ SEO.defaultProps = {
   image: null,
   pathname: null,
   article: false,
+  lang: 'hu',
+  meta: [],
 }
 
 export default SEO
